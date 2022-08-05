2a6cb6bebe367ecc59e883f4d07fbaa5954fcf66
pgeovany <pgeovany@ic.ufrj.br>
2022-08-04 23:03:46 -0300

    feat: implement a tokenMiddleware to validate any route that require authentication

---
+++ src/middlewares/tokenMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import STATUS from '../utils/statusCodes.js';
+
+async function tokenMiddleware(req, res, next) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+
+  if (!token) {
+    res.sendStatus(STATUS.UNAUTHORIZED);
+    return;
+  }
+
+  let id;
+
+  try {
+    const secret = process.env.JWT_SECRET;
+    const decoded = jwt.verify(token, secret);
+    id = decoded.id;
+  } catch (error) {
+    res.sendStatus(STATUS.UNAUTHORIZED);
+  }
+
+  req.locals = id;
+
+  next();
+}
+
+export default tokenMiddleware;
